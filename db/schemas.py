f2adfd9e6f2ba8db5e0e4347b43c5d42e4bb7e63
Alba Suria <alba.suria@upc.edu>
2026-04-02 21:03:18 +0200

    feat: schemas updated

--- db/schemas.py
+++ db/schemas.py
@@ -1,19 +1,15 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
-# 1. Base: Camps comuns que sempre es repeteixen
 class UsuariBase(BaseModel):
     nom: str
-    email: EmailStr  # Això validarà automàticament que tingui format de correu (@ i .com)
+    email: EmailStr
 
-# 2. Schema per CREAR un usuari (el que rep l'API des del frontend)
 class UsuariCreate(UsuariBase):
     password: str
 
-# 3. Schema per RETORNAR un usuari (el que l'API envia al frontend)
 class UsuariResponse(UsuariBase):
     id: int
     data_creacio: datetime
 
-    # Aquesta configuració permet que Pydantic llegeixi directament dels models de SQLAlchemy
     model_config = {"from_attributes": True}
